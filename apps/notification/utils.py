69ed3e43969be451749327ed3b417ffec6df17b3
mehdi <m69fard@gmail.com>
2024-07-29 23:06:55 +0330

    Remove redundant print

--- apps/notification/utils.py
+++ apps/notification/utils.py
@@ -6,5 +6,4 @@ from .tasks import send_sms
 def send_otp(phone_number):
     otp = random.randint(100000, 999999)
     send_sms.delay(phone_number, otp)
-    print(f"Sending OTP {otp} to {phone_number}")
     return otp
